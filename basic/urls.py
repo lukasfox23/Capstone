8ad5e491a40b9475e90162fa2d7cbc22464ccfdc
Derrek Baxter <dabaxt01@gmail.com>
2017-02-25 14:41:24 -0500

    Setup files for front-end development, some urls will need changed possibly

--- basic/urls.py
+++ basic/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^gallery/$', views.gallery, name='gallery'),
+    url(r'^conference/$', views.conference, name='conference'),
+    url(r'^account/$', views.account, name='account'),
+    url(r'^mypapers/$', views.myPapers, name='mypapers'),
+    url(r'^myconferences/$', views.myConferences, name='myconferences'),
 ]
--- basic/views.py
+++ basic/views.py
@@ -8,7 +8,29 @@ from django.contrib.auth import authenticate
 # This function will return the Login page for the user to attempt to log in.
 def login(request):
     return render(request, "basic/login.html")
-    
-@login_required(login_url='login/')
+
+def register(request):
+    return render(request, "basic/register.html")
+
+#@login_required(login_url='login/')
 def index(request):
     return render(request, "basic/basic.html")
+
+def gallery(request):
+    return render(request, "basic/gallery.html")
+
+# Thinking this url should look something like /basic/conference/"conference name"
+def conference(request):
+    return render(request, "basic/conference.html")
+
+#@login_required(login_url='login/')
+def account(request):
+    return render(request, "basic/account.html")
+
+#@login_required(login_url='login/')
+def myPapers(request):
+    return render(request, "basic/mypapers.html")
+
+#@login_required(login_url='login/')
+def myConferences(request):
+    return render(request, "basic/myconferences.html")
