4b9ed469b4bf0742b80cb7040551fbfa5e7be2c8
jtseef01 <jared.seeforth@gmail.com>
2017-04-12 16:49:29 -0400

    add bootstrap/footer, remove placeholder

--- capstone/static/js/scripts.js
+++ capstone/static/js/scripts.js
@@ -1,4 +1,4 @@
-// use a mainSlider div to create the gallery/main page effects
+// use a mainSlider div to create the main page effects
 $(document).ready(function(){
   $('.mainSlider').bxSlider({
     slideWidth: 375,
@@ -8,5 +8,16 @@ $(document).ready(function(){
     slideMargin: 20,
     auto: true,
     adaptiveHeight: true
+  });
+});
+
+// use a gallerySlider div to create the gallery page effects
+$(document).ready(function(){
+  $('.gallerySlider').bxSlider({
+    slideWidth: 375,
+    minSlides: 3,
+    maxSlides: 3,
+    moveSlides: 1,
+    slideMargin: 20,
   });
 });
